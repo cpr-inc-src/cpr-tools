4c9f8723482a1d8827786fd459d0cfbaa2782753
布野翔一 <sfuno@funoshouichinoMacBook-Pro.local>
2020-04-21 11:38:09 +0900

    ロガーの追加＆時間帯によるメンションの有無に変更

--- access_monitoring/conf/access_monitoring_conf.sh
+++ access_monitoring/conf/access_monitoring_conf.sh
@@ -7,18 +7,27 @@
 ##################################
 # 各種パス
 NOTIFICATION_SCRIPT=$SCRIPT_DIR/slack_notification.sh
+LOGGER_SCRIPT=$SCRIPT_DIR/logger.sh
+
+# ログ出力先
+TAGET_LOG_FILE=$SCRIPT_DIR/../log/access_monitoring_conf.log
 
 # 各種URL
-STDIO_URL='https://cpr-studio.jp/'
+#STDIO_URL='https://transit.yahoo.co.jp/station/rail/92427/?pref=12&done=time'
+STDIO_URL='https://cpr-studi.jp/'
 INC_URL='https://www.cpr-inc.jp/'
 SOLUTION_URL='https://www.cpr-inc.jp/solution/'
 
 # 通知関連
+NOW_TIME_HOUR=`date +%k`
+NOTIFICATION_START_OFF_HOUR_TIME=22
+NOTIFICATION_END_OFF_HOUR_TIME=8
+
 SEND_CHANNEL="#access_monitoring"
 SEND_ICON_NAME=":warning:"
 SEND_USER_NAME="監視bot"
+SEND_MENTION="@監視メンバー"
 SEND_MASSAGE_TMPLATE=$(cat <<-EOD
-<!channel>
 下記サイトがサイトダウンしています。
 状況確認並びに復旧対応をお願い致します。
 
--- access_monitoring/script/access_monitoring.sh
+++ access_monitoring/script/access_monitoring.sh
@@ -15,6 +15,10 @@ source $SCRIPT_DIR/../conf/access_monitoring_conf.sh
 ##################################
 # function
 ##################################
+logger() {
+  $LOGGER_SCRIPT -f $TAGET_LOG_FILE -t "$1"
+}
+
 access_monitoring () {
 
   # 変数の初期化
@@ -40,12 +44,29 @@ access_monitoring () {
   fi
 
   # 問い合わせ
-  status_code=`curl -LI -m 45 $url -o /dev/null -w '%{http_code}\n' -s`
+  logger "Start http request to $1"
+  status_code=`curl -LI -m 60 $url -o /dev/null -w '%{http_code}\n' -s`
+  logger "End http request to $1"
+
   if [ 200 -ne $status_code ]; then
-    error_massage=`curl -LI -m 45 $url -sS -o /dev/null 2>&1`
-    massage=`echo "$SEND_MASSAGE_TMPLATE" | sed "s|url|$url|" | sed "s|status_code|$status_code|" | sed "s|error_massage|$error_massage|"`
-    $NOTIFICATION_SCRIPT -c $SEND_CHANNEL -u $SEND_USER_NAME -m "$massage" -i $SEND_ICON_NAME
-    return 2
+    # 接続不良が多々あるためリクエストのリトライを行う。
+    logger "Retry HTTP request to $1"
+    retry_result=`curl -LI -m 60 $url -sS -w '%{http_code}\n' 2>&1`
+    status_code="$retry_result" | grep -e "[0-9]{3}\n"
+
+    if [ 200 -ne $status_code ]; then
+      error_header=`echo "$retry_result" | sed '$d'`
+      error_massage=`echo "$retry_result" | grep -e "curl: ("`
+      logger "error request Info \n $error_header"
+
+      # 深夜〜朝までは通知時にメンションを行わない（深夜開始時刻 > now && now > 朝の終了時刻）
+      massage=`echo "$SEND_MASSAGE_TMPLATE" | sed "s|url|$url|" | sed "s|status_code|$status_code|" | sed "s|error_massage|$error_massage|"`
+      if [[ $NOTIFICATION_START_OFF_HOUR_TIME -gt $NOW_TIME_HOUR ]] && [[ $NOW_TIME_HOUR -gt $NOTIFICATION_END_OFF_HOUR_TIME ]]; then
+        massage=`echo "$SEND_MENTION\n$SEND_MASSAGE_TMPLATE" | sed "s|url|$url|" | sed "s|status_code|$status_code|" | sed "s|error_massage|$error_massage|"`
+      fi
+      $NOTIFICATION_SCRIPT -c $SEND_CHANNEL -u $SEND_USER_NAME -m "$massage" -i $SEND_ICON_NAME
+      return 2
+    fi
   fi
   return 1
 }
@@ -53,6 +74,7 @@ access_monitoring () {
 ##################################
 # Main
 ##################################
+logger 'Start access monitoring process'
 # スタジオの稼働確認
 access_monitoring  'STDIO'
 # incの稼働確認
@@ -61,3 +83,4 @@ access_monitoring 'INC'
 access_monitoring 'SOLUTION'
 # 異常系テスト用
 access_monitoring '異常系'
+logger 'End access monitoring process'
